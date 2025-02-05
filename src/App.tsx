c4ecdac70a6e8bcc0b57aa083451504a57df0069
Aleksandr <ryan00@mail.ru>
2025-02-05 15:29:44 +0300

    ы

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Main } from './features/snowboards/pages/snowboard-viewer';
 
 const App = () => {
+  const [accessAllowed, setAccessAllowed] = useState(false);
+
+  useEffect(() => {
+    if (document.referrer.indexOf('tilda.cc') !== -1) {
+      setAccessAllowed(true); // Разрешаем доступ
+    }
+  }, []);
+
+  if (!accessAllowed) {
+    return <h1 className="text-center text-red-500 mt-20 text-2xl">Доступ запрещен</h1>;
+  }
   return (
     <div className=" bg-eerie-black overflow-hidden  min-w-[320px] h-screen  flex flex-col scroll-fix">
       <Main />
